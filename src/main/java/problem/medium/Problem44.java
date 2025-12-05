a0e3b509cf14ef031351f96508a3d00ac13a7311
dahye <dahye9011@naver.com>
2025-12-05 13:45:24 +0900

    feat(medium): Problem44 - filter()

--- src/main/java/problem/medium/Problem44.java
+++ src/main/java/problem/medium/Problem44.java
@@ -11,7 +11,11 @@ public class Problem44 {
      * @return 5 이상 10 이하를 제외한 숫자들의 합
      */
     public static int sumExcludingRange(List<Integer> numbers) {
-        // 여기에 코드 작성
-        return 0;
+        return numbers.stream()
+                .filter(n -> !(n >= 5 && n <= 10))
+                .mapToInt(Integer::intValue)
+                .sum();
     }
+
+
 }
