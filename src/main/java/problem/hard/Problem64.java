6bc0952f97d26f6c499e188a20bf7bac4b57cde2
dahye <dahye9011@naver.com>
2025-12-18 15:29:08 +0900

    feat(hard): Problem64 - fibonacci Set, filter(), sum()

--- src/main/java/problem/hard/Problem64.java
+++ src/main/java/problem/hard/Problem64.java
@@ -1,6 +1,8 @@
 package problem.hard;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Problem64 {
 
@@ -12,7 +14,30 @@ public class Problem64 {
      * @return 피보나치 수열에 속하는 수들의 합
      */
     public static int sumOfFibonacciNumbers(List<Integer> numbers) {
-        // 여기에 코드 작성
-        return 0;
+        int max = numbers.stream()
+                .max(Integer::compareTo)
+                .orElse(0);
+
+        Set<Integer> fibs = generateFibonacci(max);
+
+        return numbers.stream()
+                .filter(fibs::contains)
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    private static Set<Integer> generateFibonacci(int max) {
+        Set<Integer> fibs = new HashSet<>();
+        // a = 지금 보고 있는 피보나치 수, b = 그 다음 피보나치 수
+        int a = 0, b = 1;
+
+        while (a <= max) {
+            fibs.add(a);
+            int next = a + b; // 앞의 수 + 그 앞의 수 = 다음 수
+            a = b;
+            b = next;
+        }
+
+        return fibs;
     }
 }
