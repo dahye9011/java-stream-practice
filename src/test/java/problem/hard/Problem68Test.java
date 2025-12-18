383fd0b9defaa376c187bd90afdea83cdaab1235
dahye <dahye9011@naver.com>
2025-12-18 16:05:13 +0900

    fix(test): Problem68 테스트 케이스 수정

--- src/test/java/problem/hard/Problem68Test.java
+++ src/test/java/problem/hard/Problem68Test.java
@@ -11,9 +11,9 @@ public class Problem68Test {
     @Test
     public void testCalculateFrequencyAndAverage() {
         Map<Integer, String> expected = Map.of(
-                1, "Freq: 2, Avg: 2.5",
-                2, "Freq: 1, Avg: 2.5",
-                3, "Freq: 1, Avg: 2.5"
+                1, "Freq: 2, Avg: 1.75",
+                2, "Freq: 1, Avg: 1.75",
+                3, "Freq: 1, Avg: 1.75"
         );
         assertEquals(expected, Problem68.calculateFrequencyAndAverage(Arrays.asList(1, 2, 1, 3)));
     }
