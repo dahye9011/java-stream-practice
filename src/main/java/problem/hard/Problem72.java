a308896f21cec8584ef7efec76b11052595248d3
dahye <dahye9011@naver.com>
2025-12-22 23:51:56 +0900

    feat(hard): Problem72 - bitmask(), bitCount(), filter()

--- src/main/java/problem/hard/Problem72.java
+++ src/main/java/problem/hard/Problem72.java
@@ -2,6 +2,8 @@ package problem.hard;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Problem72 {
 
@@ -12,7 +14,17 @@ public class Problem72 {
      * @return 합이 짝수이고 길이가 3 이상인 모든 서브리스트의 집합
      */
     public static Set<List<Integer>> findSublistsMeetingCriteria(List<Integer> numbers) {
-        // 여기에 코드 작성
-        return null;
+        int n = numbers.size();
+
+        return IntStream.range(0, 1 << n)
+                .filter(mask -> Integer.bitCount(mask) >= 3)
+                .mapToObj(mask ->
+                        IntStream.range(0, n)
+                                .filter(i -> (mask & (1 << i)) != 0)
+                                .mapToObj(numbers::get)
+                                .toList()
+                )
+                .filter(list -> list.stream().mapToInt(Integer::intValue).sum() % 2 == 0)
+                .collect(Collectors.toSet());
     }
 }
